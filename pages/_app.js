9ae78e074b7b2245ddde34127de2e25c1575b6de
jacobNar <jdnarayan56@gmail.com>
2023-11-30 17:57:08 -0600

    code cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.css';
-import 'prismjs/themes/prism-tomorrow.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <span className="theme-bejamas" />
       <Component {...pageProps} />
     </>
   );

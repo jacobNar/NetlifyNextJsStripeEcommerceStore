f187e225e7660f07b2780395dff56a6815ff961a
jacobNar <jdnarayan56@gmail.com>
2023-12-04 19:09:48 -0600

    add back in shop page

--- pages/shop.js
+++ pages/shop.js
@@ -2,40 +2,39 @@ import React from 'react'
 import Products from '../components/Products';
 import Header from '../components/Header';
 
-// export async function getStaticProps() {
-//     // Fetch data for the dynamic route using the params object
-//     // var baseUrl = 'http://localhost:8888';
-//     var baseUrl = 'https://ecomtemplate1.netlify.app'
-//     const products = await fetch(`${baseUrl}/.netlify/functions/getProducts`)
-//     const prices = await fetch(`${baseUrl}/.netlify/functions/getPrices`);
+export async function getStaticProps() {
+    // Fetch data for the dynamic route using the params object
+    // var baseUrl = 'http://localhost:8888';
+    var baseUrl = 'https://ecomtemplate1.netlify.app'
+    const products = await fetch(`${baseUrl}/.netlify/functions/getProducts`)
+    const prices = await fetch(`${baseUrl}/.netlify/functions/getPrices`);
 
-//     var productsData = await products.json();
-//     var pricesData = await prices.json()
+    var productsData = await products.json();
+    var pricesData = await prices.json()
 
-//     var pricesDict = {}
+    var pricesDict = {}
 
-//     for (let i = 0; i < pricesData.length; i++){
-//         pricesDict[pricesData[i].id] = pricesData[i].unit_amount
-//     }
+    for (let i = 0; i < pricesData.length; i++){
+        pricesDict[pricesData[i].id] = pricesData[i].unit_amount
+    }
     
 
-//     return {
-//       props: {
-//         products: productsData,
-//         prices: pricesDict
-//       },
-//     }
-//   }
+    return {
+      props: {
+        products: productsData,
+        prices: pricesDict
+      },
+    }
+  }
 
-//{products, prices}
-export default function Shop() {
+export default function Shop({products, prices}) {
   return (
     <>
-    {/* <Header style={1} />
+    <Header style={1} />
     <main className='width-container'>
         <h1>Shop</h1>
         <Products products={products} prices={prices} />
-    </main> */}
+    </main>
     </>
     
   )
